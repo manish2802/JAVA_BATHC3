04c1bb7c35b88b00f17d7d5d7119323116251135
manish2802 <manishverma.java@gmail.com>
2019-05-13 23:57:15 +0530

    Java

---
+++ JAVA/COLLECTION/src/main/java/list/arraylist/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package list.arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String... k) {
+		ArrayList<String> list = new ArrayList<String>();
+		//list.add(1);
+		list.add("manish");
+		System.out.println(list);
+
+	}
+
+}
---
+++ JAVA/INTERVIEW_PROGRAMS/src/main/java/com/asthvinayak/java/B.java
@@ -0,0 +1,15 @@
+package com.asthvinayak.java;
+
+public class B implements A {
+
+	
+	public  B show() {
+		
+		return new B();
+	}
+
+	
+	
+	
+
+}
