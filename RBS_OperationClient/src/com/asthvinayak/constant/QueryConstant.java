5a6c2d231f4a34acd28324e149dbeed0426d08bb
manish2802 <manishverma.java@gmail.com>
2019-07-07 15:51:53 +0530

    Query constant

---
+++ RBS_OperationClient/src/com/asthvinayak/constant/QueryConstant.java
@@ -0,0 +1,10 @@
+package com.asthvinayak.constant;
+
+public class QueryConstant {
+
+	public static String GET_LOGIN = new StringBuilder("SELECT USER_NAME AS UNAME,PASSWORD AS PWD FROM C_LOGIN")
+			.append(" WHERE USER_NAME=? AND PASSWORD=?").toString();
+
+	public static String GET_TX = new StringBuilder("").append(" ").toString();
+
+}
